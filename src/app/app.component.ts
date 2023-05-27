7f8914b2fc2aeee3d0b2531bf9673ee471c45718
Gonzalo Barrazueta <68360681+gonzalobarrazueta@users.noreply.github.com>
2023-05-26 21:19:57 -0500

    Create and moved Navbar to its own component

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -2,11 +2,12 @@ import { Component } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { RouterLink, RouterOutlet} from '@angular/router';
 import { SignUpComponent } from "./sign-up/sign-up.component";
+import { NavbarComponent } from "./navbar/navbar.component";
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [CommonModule, RouterOutlet, SignUpComponent, RouterLink],
+  imports: [CommonModule, RouterOutlet, SignUpComponent, RouterLink, NavbarComponent],
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.scss']
 })
