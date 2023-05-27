ffa105b7c115b9bbb417a973dbd82d0e4284c0f6
Gonzalo Barrazueta <68360681+gonzalobarrazueta@users.noreply.github.com>
2023-05-26 21:08:44 -0500

    Add routing

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,11 +1,12 @@
 import { Component } from '@angular/core';
 import { CommonModule } from '@angular/common';
-import { RouterOutlet } from '@angular/router';
+import { RouterLink, RouterOutlet} from '@angular/router';
+import { SignUpComponent } from "./sign-up/sign-up.component";
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [CommonModule, RouterOutlet],
+  imports: [CommonModule, RouterOutlet, SignUpComponent, RouterLink],
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.scss']
 })
