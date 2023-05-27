a03b5c9a654c75da4827cb2d2919d2ce4bb7ee16
Gonzalo Barrazueta <68360681+gonzalobarrazueta@users.noreply.github.com>
2023-05-27 13:09:13 -0500

    Create Sign In component and add route

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,6 +1,8 @@
 import { Routes } from '@angular/router';
 import { SignUpComponent } from "./sign-up/sign-up.component";
+import { SignInComponent } from "./sign-in/sign-in.component";
 
 export const routes: Routes = [
-  { path: 'sign-up', component: SignUpComponent, title: 'Registro' }
+  { path: 'sign-up', component: SignUpComponent, title: 'Registro' },
+  { path: 'sign-in', component: SignInComponent, title: 'Iniciar sesión' }
 ];
