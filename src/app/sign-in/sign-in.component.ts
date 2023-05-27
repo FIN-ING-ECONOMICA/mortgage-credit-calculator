55b30e78498e3dedcee2a2d0e0d0ab0f793e2dd5
Gonzalo Barrazueta <68360681+gonzalobarrazueta@users.noreply.github.com>
2023-05-27 13:48:00 -0500

    Add form group

--- src/app/sign-in/sign-in.component.ts
+++ src/app/sign-in/sign-in.component.ts
@@ -1,13 +1,18 @@
 import { Component } from '@angular/core';
 import { CommonModule } from '@angular/common';
+import { FormControl, FormGroup, FormsModule, ReactiveFormsModule } from "@angular/forms";
 
 @Component({
   selector: 'app-sign-in',
   standalone: true,
-  imports: [CommonModule],
+  imports: [CommonModule, FormsModule, ReactiveFormsModule],
   templateUrl: './sign-in.component.html',
   styleUrls: ['./sign-in.component.scss']
 })
 export class SignInComponent {
+  signInForm = new FormGroup({
+    email: new FormControl(''),
+    password: new FormControl('')
+  })
 
 }
