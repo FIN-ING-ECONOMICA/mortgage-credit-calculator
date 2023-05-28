c442877f77bca6714871f44505ccb3164af16f8a
Gonzalo Barrazueta <68360681+gonzalobarrazueta@users.noreply.github.com>
2023-05-28 14:06:12 -0500

    Create submit form function

--- src/app/components/sign-up/sign-up.component.ts
+++ src/app/components/sign-up/sign-up.component.ts
@@ -1,6 +1,8 @@
 import { Component } from '@angular/core';
 import { CommonModule } from '@angular/common';
-import { FormControl, FormGroup, ReactiveFormsModule} from "@angular/forms";
+import { FormControl, FormGroup, ReactiveFormsModule } from "@angular/forms";
+import { AuthService } from "../../services/auth.service";
+import { User } from "../../models/user";
 
 @Component({
   selector: 'app-sign-up',
@@ -14,9 +16,27 @@ export class SignUpComponent {
     firstName: new FormControl(''),
     lastName: new FormControl(''),
     email: new FormControl(''),
-    phoneNumber: new FormControl(''),
-    address: new FormControl(''),
     password: new FormControl(''),
+    phone: new FormControl(''),
     birthdate: new FormControl(''),
-  })
+    homeAddress: new FormControl('')
+  });
+
+  constructor(private authService: AuthService) {
+  }
+
+  submitForm() {
+    let user: User = {
+      name: this.signUpForm.value.firstName ?? '',
+      lastName: this.signUpForm.value.lastName ?? '',
+      email: this.signUpForm.value.email ?? '',
+      password: this.signUpForm.value.password ?? '',
+      phone: this.signUpForm.value.phone ?? '',
+      birthdate: new Date(this.signUpForm.value.birthdate ?? ''),
+      homeAddress: this.signUpForm.value.homeAddress ?? ''
+    }
+
+    this.authService.createUser(user)
+      .subscribe(console.log);
+  }
 }
