c403a13e220d70c9310a64e536e41f7f7be8053e
Gonzalo Barrazueta <68360681+gonzalobarrazueta@users.noreply.github.com>
2023-05-28 13:53:22 -0500

    Create method to create a user

--- src/app/services/auth.service.ts
+++ src/app/services/auth.service.ts
@@ -1,9 +1,16 @@
 import { Injectable } from '@angular/core';
+import { HttpClient } from "@angular/common/http";
+import { User } from "../models/user";
 
 @Injectable({
   providedIn: 'root'
 })
 export class AuthService {
+  signUpUrl = 'http://localhost:3001/signup'
 
-  constructor() { }
+  constructor(private http: HttpClient) { }
+
+  createUser(user: User) {
+    return this.http.post(this.signUpUrl, user);
+  }
 }
