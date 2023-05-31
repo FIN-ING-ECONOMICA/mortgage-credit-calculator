5da245dc237bd2bfe185a3a2118935da48921594
Gonzalo Barrazueta <68360681+gonzalobarrazueta@users.noreply.github.com>
2023-05-31 16:09:04 -0500

    Add logic to check if user is logged in

--- src/app/services/auth.service.ts
+++ src/app/services/auth.service.ts
@@ -1,16 +1,28 @@
 import { Injectable } from '@angular/core';
 import { HttpClient } from "@angular/common/http";
 import { User } from "../models/user";
+import { BehaviorSubject, tap } from "rxjs";
 
 @Injectable({
   providedIn: 'root'
 })
 export class AuthService {
   signUpUrl = 'http://localhost:3001/signup'
+  private _isLoggedIn$ = new BehaviorSubject<boolean>(false);
+  isLoggedIn$ = this._isLoggedIn$.asObservable()
 
-  constructor(private http: HttpClient) { }
+  constructor(private http: HttpClient) {
+    const token = localStorage.getItem('token');
+    this._isLoggedIn$.next(!!token);
+  }
 
   createUser(user: User) {
-    return this.http.post(this.signUpUrl, user);
+    return this.http.post(this.signUpUrl, user)
+      .pipe(
+        tap((response: any) => {
+          localStorage.setItem('token', response.token);
+          this._isLoggedIn$.next(true);
+        })
+    );
   }
 }
