40a83e3350ec18d6d3515441f42b67bc600fa6cc
Gonzalo Barrazueta <68360681+gonzalobarrazueta@users.noreply.github.com>
2023-05-31 21:27:13 -0500

    Create form group

--- src/app/components/loan-application-form/loan-application-form.component.ts
+++ src/app/components/loan-application-form/loan-application-form.component.ts
@@ -1,13 +1,28 @@
 import { Component } from '@angular/core';
 import { CommonModule } from '@angular/common';
+import { FormControl, FormGroup, ReactiveFormsModule } from "@angular/forms";
 
 @Component({
   selector: 'app-loan-application-form',
   standalone: true,
-  imports: [CommonModule],
+  imports: [CommonModule, ReactiveFormsModule],
   templateUrl: './loan-application-form.component.html',
   styleUrls: ['./loan-application-form.component.scss']
 })
 export class LoanApplicationFormComponent {
-
+  loanForm = new FormGroup({
+    realStatePrice: new FormControl(''),
+    initialPayment: new FormControl(''),
+    paymentFrequency: new FormControl('')
+  })
+  paymentFrequency = [
+    "Diario",
+    "Quincenal",
+    "Mensual",
+    "Bimestral",
+    "Trimestral",
+    "Cuatrimestral",
+    "Semestral",
+    "Anual"
+  ]
 }
