ca98a658c5f46f1c6a48dcb3403cb816cfc4439b
Gonzalo Barrazueta <68360681+gonzalobarrazueta@users.noreply.github.com>
2023-05-31 19:42:50 -0500

    Define empty route

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -3,6 +3,7 @@ import { SignUpComponent } from "./components/sign-up/sign-up.component";
 import { SignInComponent } from "./components/sign-in/sign-in.component";
 
 export const routes: Routes = [
+  { path: '', redirectTo: '/', pathMatch: 'full' },
   { path: 'sign-up', component: SignUpComponent, title: 'Registro' },
   { path: 'sign-in', component: SignInComponent, title: 'Iniciar sesión' }
 ];
