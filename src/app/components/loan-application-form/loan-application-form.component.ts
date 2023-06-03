0a088ecf627f9b378a20213f505ec5be225863dd
Gonzalo Barrazueta <68360681+gonzalobarrazueta@users.noreply.github.com>
2023-06-02 23:26:43 -0500

    Add form controls and method to calculate the initial payment

--- src/app/components/loan-application-form/loan-application-form.component.ts
+++ src/app/components/loan-application-form/loan-application-form.component.ts
@@ -1,6 +1,6 @@
 import { Component } from '@angular/core';
 import { CommonModule } from '@angular/common';
-import { FormControl, FormGroup, ReactiveFormsModule } from "@angular/forms";
+import { FormControl, FormGroup, ReactiveFormsModule, Validators } from "@angular/forms";
 
 @Component({
   selector: 'app-loan-application-form',
@@ -10,10 +10,14 @@ import { FormControl, FormGroup, ReactiveFormsModule } from "@angular/forms";
   styleUrls: ['./loan-application-form.component.scss']
 })
 export class LoanApplicationFormComponent {
-  loanForm = new FormGroup({
-    realStatePrice: new FormControl(''),
-    initialPayment: new FormControl(''),
-    paymentFrequency: new FormControl('')
+  loanForm   = new FormGroup({
+    realStatePrice: new FormControl('', [Validators.required, Validators.min(0)]),
+    initialPaymentPercentage: new FormControl('', [Validators.required, Validators.min(0)]),
+    tea: new FormControl(''),
+    paymentFrequency: new FormControl(''),
+    years: new FormControl(''),
+    paymentPeriod: new FormControl(''),
+    initialPayment: new FormControl('')
   })
   paymentFrequency: Record<string, number> = {
     Diario: 1,
@@ -25,5 +29,15 @@ export class LoanApplicationFormComponent {
     Semestral: 180,
     Anual: 360
   }
-  frequencies = Object.keys(this.paymentFrequency)
+  frequencies = Object.keys(this.paymentFrequency);
+
+  calculateInitialPayment(): string {
+    let percentage = Number(this.loanForm.get('initialPaymentPercentage')?.value)
+    let realStatePrice = Number(this.loanForm.get('realStatePrice')?.value)
+    return (realStatePrice * percentage / 100).toFixed(2).toString()
+  }
+
+  renderInitialPayment(): string {
+    return `S/ ${this.calculateInitialPayment()}`
+  }
 }
