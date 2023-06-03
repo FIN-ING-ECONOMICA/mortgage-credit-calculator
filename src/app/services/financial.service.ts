3c1b1bed6569b661358d7e6019626963ab45a34e
Gonzalo Barrazueta <68360681+gonzalobarrazueta@users.noreply.github.com>
2023-06-03 11:15:44 -0500

    Add function to calculate the loan amount

--- src/app/services/financial.service.ts
+++ src/app/services/financial.service.ts
@@ -8,6 +8,11 @@ export class FinancialService {
   constructor() { }
 
   calculateInitialPayment(percentage: number, realStatePrice: number): number {
-    return (realStatePrice * percentage / 100)
+    return realStatePrice * percentage / 100
+  }
+
+  calculateLoan(percentage: number, realStatePrice: number) {
+    let initialPayment = this.calculateInitialPayment(percentage, realStatePrice)
+    return realStatePrice - initialPayment
   }
 }
