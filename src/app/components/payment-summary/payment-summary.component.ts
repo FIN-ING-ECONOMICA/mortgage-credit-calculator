3d3b64cd82df96020095023bd1f8c6638cfc7990
Gonzalo Barrazueta <68360681+gonzalobarrazueta@users.noreply.github.com>
2023-06-05 18:55:20 -0500

    Create Payment Summary component and add route

---
+++ src/app/components/payment-summary/payment-summary.component.ts
@@ -0,0 +1,13 @@
+import { Component } from '@angular/core';
+import { CommonModule } from '@angular/common';
+
+@Component({
+  selector: 'app-payment-summary',
+  standalone: true,
+  imports: [CommonModule],
+  templateUrl: './payment-summary.component.html',
+  styleUrls: ['./payment-summary.component.scss']
+})
+export class PaymentSummaryComponent {
+
+}
