1076f8983143782f5dc95cfd4a3f443bda82eeb8
Gonzalo Barrazueta <68360681+gonzalobarrazueta@users.noreply.github.com>
2023-06-05 23:30:24 -0500

    Add Loan model

---
+++ src/app/models/loan.ts
@@ -0,0 +1,10 @@
+export interface Loan {
+  realStatePrice: number,
+  initialPaymentPercentage: number,
+  initialPayment: number,
+  tea: number,
+  tep: number,
+  paymentFrequency: { [key: string]: number },
+  years: number,
+  period: number
+}
