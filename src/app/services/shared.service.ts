2a1dcb2fb7d94106460999b4d3e5d96a9b2eb4ba
Gonzalo Barrazueta <68360681+gonzalobarrazueta@users.noreply.github.com>
2023-06-05 23:31:34 -0500

    Add type

--- src/app/services/shared.service.ts
+++ src/app/services/shared.service.ts
@@ -1,11 +1,21 @@
 import { Injectable } from '@angular/core';
+import { Loan } from "../models/loan";
 
 @Injectable({
   providedIn: 'root'
 })
 export class SharedService {
 
-  loan: any;
+  loan: Loan = {
+    realStatePrice: 0,
+    initialPaymentPercentage: 0,
+    initialPayment: 0,
+    tea: 0,
+    tep: 0,
+    paymentFrequency: { ['']: 0 },
+    years: 0,
+    period: 0
+  }
 
   constructor() { }
 }
