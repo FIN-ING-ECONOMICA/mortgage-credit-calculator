fd7ea9170b3d0dda46b85e210509a77528077ea6
Gonzalo Barrazueta <68360681+gonzalobarrazueta@users.noreply.github.com>
2023-06-07 18:11:53 -0500

    Add methods to get Loan values

--- src/app/components/payment-summary/payment-summary.component.ts
+++ src/app/components/payment-summary/payment-summary.component.ts
@@ -1,5 +1,6 @@
 import { Component } from '@angular/core';
 import { CommonModule } from '@angular/common';
+import { SharedService } from "../../services/shared.service";
 
 @Component({
   selector: 'app-payment-summary',
@@ -10,4 +11,25 @@ import { CommonModule } from '@angular/common';
 })
 export class PaymentSummaryComponent {
 
+  headersAndValues = {}
+
+  constructor(private sharedService: SharedService) {
+    console.log('Payment Summary:', this.sharedService.loan)
+    this.headersAndValues = this.getLoanValues()
+  }
+
+  getLoanValues() {
+    return {
+      'Precio de Venta': 'S/' + this.sharedService.loan.realStatePrice,
+      '% Cuota Inicial': this.sharedService.loan.initialPaymentPercentage + '%',
+      'Préstamo': 'S/' + this.sharedService.loan.tea,
+      'Frecuencia': this.getFrequencyKey(this.sharedService.loan.paymentFrequency),
+      'Cantidad de años': this.sharedService.loan.years,
+      'Cantidad de periodos': this.sharedService.loan.periods
+    }
+  }
+
+  getFrequencyKey(frequency: object) {
+    return Object.keys(frequency)[0]
+  }
 }
