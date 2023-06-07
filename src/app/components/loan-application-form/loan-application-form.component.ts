62f64804b24bbede4c8b2122f761ad2d708858ad
Gonzalo Barrazueta <68360681+gonzalobarrazueta@users.noreply.github.com>
2023-06-07 17:38:27 -0500

    Add method get the form values and assign them to the Loan in the Shared Service

--- src/app/components/loan-application-form/loan-application-form.component.ts
+++ src/app/components/loan-application-form/loan-application-form.component.ts
@@ -3,6 +3,8 @@ import { CommonModule } from '@angular/common';
 import { FormControl, FormGroup, ReactiveFormsModule, Validators } from "@angular/forms";
 import { FinancialService } from "../../services/financial.service";
 import { SharedService } from "../../services/shared.service";
+import { Router } from "@angular/router";
+import { Loan } from "../../models/loan";
 
 @Component({
   selector: 'app-loan-application-form',
@@ -37,19 +39,32 @@ export class LoanApplicationFormComponent {
   }
   frequencies = Object.keys(this.paymentFrequency);
 
-  constructor(private financialService: FinancialService, private sharedService: SharedService) {
+  constructor(private financialService: FinancialService, private sharedService: SharedService,
+              private router: Router) {
   }
 
   submitForm() {
-    let loan = {
-      realStatePrice: this.loanForm.value.realStatePrice ?? '',
-      initialPaymentPercentage: this.loanForm.value.initialPaymentPercentage ?? '',
-      tea: this.loanForm.value.tea ?? '',
-      paymentFrequency: this.paymentFrequency[this.loanForm.value.paymentFrequency ?? ''],
-      years: this.loanForm.value.years ?? ''
-    }
+    this.sharedService.loan = this.assignLoanValues();
+    console.log('Cálculo del préstamo:', this.sharedService.loan)
+    this.router.navigate(['/payment-summary'])
+  }
 
-    console.log(loan)
-    this.sharedService.loan = loan;
+  assignLoanValues(): Loan {
+    let loan: Loan = {
+      realStatePrice: Number(this.loanForm.value.realStatePrice) ?? 0,
+      initialPaymentPercentage: Number(this.loanForm.value.initialPaymentPercentage) ?? 0,
+      initialPayment: this.financialService.calculateLoan(Number(this.loanForm.value.initialPaymentPercentage) ?? 0, Number(this.loanForm.value.realStatePrice) ?? 0),
+      tea: Number(this.loanForm.value.tea) ?? 0,
+      tep: this.convertTepTo7Decimals(Number(this.loanForm.value.tea) ?? 0, this.paymentFrequency[this.loanForm.value.paymentFrequency ?? '']),
+      paymentFrequency: { [this.loanForm.value.paymentFrequency ?? '']: this.paymentFrequency[this.loanForm.value.paymentFrequency ?? ''] },
+      years: Number(this.loanForm.value.years) ?? 0,
+      period: this.financialService.getPeriod(this.paymentFrequency[this.loanForm.value.paymentFrequency ?? ''], Number(this.loanForm.value.years) ?? 0)
+    }
+    return loan
+  }
+
+  convertTepTo7Decimals(tea: number, _paymentFrequency: number): number {
+    let tep = (this.financialService.teaToTep(tea, _paymentFrequency) * 100).toFixed(7)
+    return Number(tep)
   }
 }
