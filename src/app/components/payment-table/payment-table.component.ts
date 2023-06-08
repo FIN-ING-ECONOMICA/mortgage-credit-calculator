856c9d6598009737260514015807c3afa69412f5
Gonzalo Barrazueta <68360681+gonzalobarrazueta@users.noreply.github.com>
2023-06-07 20:32:17 -0500

    Add HTML structure and table data

--- src/app/components/payment-table/payment-table.component.ts
+++ src/app/components/payment-table/payment-table.component.ts
@@ -1,5 +1,6 @@
 import { Component } from '@angular/core';
 import { CommonModule } from '@angular/common';
+import { PeriodicPayment } from "../../models/periodic-payment";
 
 @Component({
   selector: 'app-payment-table',
@@ -10,4 +11,17 @@ import { CommonModule } from '@angular/common';
 })
 export class PaymentTableComponent {
 
+  tableData: Array<PeriodicPayment> = [
+    { paymentIndex: 1, initialBalance: 1400000, finalBalance: 1285950.02, tea: 9, tep: 4.4030651, interestAmount: 63404.14, periodicPayment: 217454.11, amortization: 154049.98, paymentFrequency: { 'Semestral': 180 }, periods: 8 }
+  ]
+  tableHeaders: Array<string> = [
+    'N°',
+    'TEA',
+    'TEP',
+    'Saldo Inicial',
+    'Interés',
+    'Cuota',
+    'Amortización',
+    'Saldo Final'
+  ]
 }
