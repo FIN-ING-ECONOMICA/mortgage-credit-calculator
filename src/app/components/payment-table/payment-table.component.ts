4390f7f5699c37413bb7a527d82d52dcc5a696bf
Gonzalo Barrazueta <68360681+gonzalobarrazueta@users.noreply.github.com>
2023-06-08 00:34:48 -0500

    Calculate table data

--- src/app/components/payment-table/payment-table.component.ts
+++ src/app/components/payment-table/payment-table.component.ts
@@ -1,6 +1,9 @@
 import { Component } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { PeriodicPayment } from "../../models/periodic-payment";
+import { SharedService } from "../../services/shared.service";
+import { Loan } from "../../models/loan";
+import { FinancialService } from "../../services/financial.service";
 
 @Component({
   selector: 'app-payment-table',
@@ -11,9 +14,7 @@ import { PeriodicPayment } from "../../models/periodic-payment";
 })
 export class PaymentTableComponent {
 
-  tableData: Array<PeriodicPayment> = [
-    { paymentIndex: 1, initialBalance: 1400000, finalBalance: 1285950.02, tea: 9, tep: 4.4030651, interestAmount: 63404.14, periodicPayment: 217454.11, amortization: 154049.98, paymentFrequency: { 'Semestral': 180 }, periods: 8 }
-  ]
+  tableData: Array<PeriodicPayment> = []
   tableHeaders: Array<string> = [
     'N°',
     'TEA',
@@ -24,4 +25,61 @@ export class PaymentTableComponent {
     'Amortización',
     'Saldo Final'
   ]
+
+  constructor(private sharedService: SharedService, private financialService: FinancialService) {
+    this.tableData = this.calculateTableData()
+  }
+
+  calculateTableData(): Array<PeriodicPayment> {
+    let _periodicPayment: PeriodicPayment = this.convertLoanToPeriodicPayment(this.sharedService.loan)
+    let arraySize = _periodicPayment.periods;
+    let _tableData: Array<PeriodicPayment> = []
+
+    for (let i = 0; i < arraySize; i++) {
+
+      if (i > 0) {
+        _periodicPayment = _tableData[i - 1]
+      }
+
+      let initialBalance = this.roundTo2Decimals(this.financialService.calculateFinalBalance(_periodicPayment.initialBalance, _periodicPayment.amortization))
+      let interestAmount = this.roundTo2Decimals(this.financialService.calculateInterestAmount(initialBalance, _periodicPayment.tep))
+      let periodicPayment = this.roundTo2Decimals(this.financialService.calculatePeriodicPayment(initialBalance, _periodicPayment.tep, _periodicPayment.periods, i + 1))
+      let amortization = this.roundTo2Decimals(this.financialService.calculateAmortization(periodicPayment, interestAmount))
+      let finalBalance = this.roundTo2Decimals(this.financialService.calculateFinalBalance(initialBalance, amortization))
+
+      _tableData.push({
+        paymentIndex: i + 1,
+        initialBalance: initialBalance,
+        finalBalance: finalBalance,
+        tea: _periodicPayment.tea,
+        tep: _periodicPayment.tep,
+        interestAmount: interestAmount,
+        periodicPayment: periodicPayment,
+        amortization: amortization,
+        paymentFrequency: _periodicPayment.paymentFrequency,
+        periods: _periodicPayment.periods
+      })
+    }
+    return _tableData
+  }
+
+  convertLoanToPeriodicPayment(loan: Loan): PeriodicPayment {
+    let periodicPayment: PeriodicPayment = {
+      paymentIndex: 0,
+      initialBalance: loan.initialPayment,
+      finalBalance: 0,
+      tea: loan.tea,
+      tep: loan.tep,
+      interestAmount: 0,
+      periodicPayment: 0,
+      amortization: 0,
+      paymentFrequency: loan.paymentFrequency,
+      periods: loan.periods
+    }
+    return periodicPayment
+  }
+
+  roundTo2Decimals(num: number) {
+    return Number(num.toFixed(2))
+  }
 }
