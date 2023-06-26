b2f0e33e4eaa9d9d280fcc1e5778c7504e9bcebe
Gonzalo Barrazueta <68360681+gonzalobarrazueta@users.noreply.github.com>
2023-06-26 12:19:19 -0500

    Create component to display the VAN and TIR and add its route

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -3,11 +3,13 @@ import { SignUpComponent } from "./components/sign-up/sign-up.component";
 import { SignInComponent } from "./components/sign-in/sign-in.component";
 import { LoanApplicationFormComponent } from "./components/loan-application-form/loan-application-form.component";
 import { PaymentSummaryComponent } from "./components/payment-summary/payment-summary.component";
+import { VanTirComponent } from "./components/van-tir/van-tir.component";
 
 export const routes: Routes = [
   { path: '', redirectTo: '/', pathMatch: 'full' },
   { path: 'sign-up', component: SignUpComponent, title: 'Registro' },
   { path: 'sign-in', component: SignInComponent, title: 'Iniciar sesión' },
   { path: 'loan-application', component: LoanApplicationFormComponent, title: 'Solicitud de préstamo' },
-  { path: 'payment-summary', component: PaymentSummaryComponent, title: 'Plan de pagos' }
+  { path: 'payment-summary', component: PaymentSummaryComponent, title: 'Plan de pagos' },
+  { path: 'van-tir', component: VanTirComponent, title: 'VAN y TIR'}
 ];
---
+++ src/app/components/van-tir/van-tir.component.ts
@@ -0,0 +1,13 @@
+import { Component } from '@angular/core';
+import { CommonModule } from '@angular/common';
+
+@Component({
+  selector: 'app-van-tir',
+  standalone: true,
+  imports: [CommonModule],
+  templateUrl: './van-tir.component.html',
+  styleUrls: ['./van-tir.component.scss']
+})
+export class VanTirComponent {
+
+}
